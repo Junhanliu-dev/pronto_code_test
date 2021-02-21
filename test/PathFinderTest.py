e0b433e30abdfd4285d0be5081842bff9340602f
junhanliu <junhanliu.dev@gmail.com>
2021-02-22 10:11:27 +1100

    added mark down

--- test/PathFinderTest.py
+++ test/PathFinderTest.py
@@ -1,6 +1,6 @@
 import unittest
 
-from helpers.DefaultPathFinder import PathFinderHelper
+from helpers.DefaultPathFinder import DefaultPathFinder
 from utils.Coordination import Coordination
 from utils.Direction import Direction
 
@@ -12,7 +12,7 @@ class TestPathFinder(unittest.TestCase):
         current_position: Coordination = Coordination(3, 2)
         current_direction: Direction = Direction.NORTH
 
-        result = PathFinderHelper.find_minimum_path_with_turn_amount(start_coord, current_position, current_direction)
+        result = DefaultPathFinder.find_minimum_path_with_turn_amount(start_coord, current_position, current_direction)
         self.assertEqual(result, 7)
 
     def test_path_2(self):
@@ -20,10 +20,13 @@ class TestPathFinder(unittest.TestCase):
         current_position: Coordination = Coordination(-2, 2)
         current_direction: Direction = Direction.EAST
 
-        result = PathFinderHelper.find_minimum_path_with_turn_amount(start_coord, current_position, current_direction)
+        result = DefaultPathFinder.find_minimum_path_with_turn_amount(start_coord, current_position, current_direction)
         self.assertEqual(result, 5)
 
     def test_path_3(self):
         start_coord = Coordination(-5, -4)
         current_coord = Coordination(-2, -2)
-        print(PathFinderHelper.find_path(start_coord, current_coord))
+        correct_path = [Coordination(-2, -2), Coordination(-3, -2),
+                        Coordination(-4, -2), Coordination(-5, -2),
+                        Coordination(-5, -3), Coordination(-5, -4)]
+        self.assertListEqual(DefaultPathFinder.find_path(start_coord, current_coord), correct_path)
